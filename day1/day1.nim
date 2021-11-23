bff029980d1f95d50f7cd2c9aac8a8aab5c6791d
Jeremy Miller <jeremymiller@fastmail.com>
2021-11-22 16:11:53 -0800

    Add day 1

---
+++ day1/day1.nim
@@ -0,0 +1,21 @@
+import strutils
+import sequtils
+import std/strformat
+
+let data = readFile("day1/data1")
+let ns = data.splitLines.filterIt(it.len > 0).map(parseInt)
+
+block part1:
+    for x in ns:
+        for y in ns:
+            if x + y == 2020:
+                echo fmt"{x} * {y} = {x * y}"
+                break part1
+
+block part2:
+    for x in ns:
+        for y in ns:
+            for z in ns:
+                if x + y + z == 2020:
+                    echo fmt"{x} * {y} * {z} = {x * y * z}"
+                    break part2
