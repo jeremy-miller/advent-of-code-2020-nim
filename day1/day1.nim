8c7f8d51210e4f4bebd16b0a2e10b67aca983767
Jeremy Miller <jeremymiller@fastmail.com>
2021-11-23 17:11:41 -0800

    Use 2 spaces

--- day1/day1.nim
+++ day1/day1.nim
@@ -6,16 +6,16 @@ let data = readFile("day1/data")
 let ns = data.splitLines.filterIt(it.len > 0).map(parseInt)
 
 block part1:
-    for x in ns:
-        for y in ns:
-            if x + y == 2020:
-                echo fmt"Part 1: {x} * {y} = {x * y}"
-                break part1
+  for x in ns:
+    for y in ns:
+      if x + y == 2020:
+        echo fmt"Part 1: {x} * {y} = {x * y}"
+        break part1
 
 block part2:
-    for x in ns:
-        for y in ns:
-            for z in ns:
-                if x + y + z == 2020:
-                    echo fmt"Part 2: {x} * {y} * {z} = {x * y * z}"
-                    break part2
+  for x in ns:
+    for y in ns:
+      for z in ns:
+        if x + y + z == 2020:
+          echo fmt"Part 2: {x} * {y} * {z} = {x * y * z}"
+          break part2
--- day3/day3.nim
+++ day3/day3.nim
@@ -3,23 +3,23 @@ import strutils
 import std/strformat
 
 let
-    data = readFile("day3/data")
-    grid = data.splitLines.filterIt(it.len > 0)
-    rows = len(grid)
-    cols = len(grid[0])
+  data = readFile("day3/data")
+  grid = data.splitLines.filterIt(it.len > 0)
+  rows = len(grid)
+  cols = len(grid[0])
 
 proc countTrees(dx, dy: int): int =
-    var
-        count = 0
-        x = 0
-        y = 0
-    while y < rows:
-        let row = grid[y]
-        if row[x] == '#':
-            count += 1
-        x = (x + dx) mod cols
-        y += dy
-    return count
+  var
+    count = 0
+    x = 0
+    y = 0
+  while y < rows:
+    let row = grid[y]
+    if row[x] == '#':
+      count += 1
+    x = (x + dx) mod cols
+    y += dy
+  return count
 
 let treeCount = countTrees(3, 1)
 
@@ -36,6 +36,6 @@ const slopes = [
 var allTreeCount = 1
 
 for slope in slopes:
-    allTreeCount *= countTrees(slope.dx, slope.dy)
+  allTreeCount *= countTrees(slope.dx, slope.dy)
 
 echo fmt"Part 2: {allTreeCount}"
