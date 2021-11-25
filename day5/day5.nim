83e639e11eef8b49cce7e004b532966277010441
Jeremy Miller <jeremymiller@fastmail.com>
2021-11-25 10:43:04 -0800

    Add day 5

---
+++ day5/day5.nim
@@ -0,0 +1,58 @@
+import sequtils
+import strutils
+import std/math, sequtils, strformat
+
+const
+  rowChars = ['F', 'B']
+  seatChars = ['L', 'R']
+
+let boardingPasses = readFile("day5/data").splitLines.filterIt(it.len > 0)
+
+func binarySpacePartition(min, max: int, chr: char, chars: array[2, char]): tuple[newMin, newMax: int] =
+  let mid = ceilDiv((max - min), 2)
+  if chr == chars[0]:
+    return (min, max - mid)
+  else:
+    return (min + mid, max)
+
+var largestSeatID = 0
+for boardingPass in boardingPasses:
+  var
+    rowMin = 0
+    rowMax = 127
+    seatMin = 0
+    seatMax = 7
+  for chr in boardingPass[0 ..< ^3]:
+    (rowMin, rowMax) = binarySpacePartition(rowMin, rowMax, chr, rowChars)
+  let row = rowMin
+  for chr in boardingPass[^3 .. ^1]:
+    (seatMin, seatMax) = binarySpacePartition(seatMin, seatMax, chr, seatChars)
+  let
+    seat = seatMin
+    seatID = row * 8 + seat
+  if seatID > largestSeatID:
+    largestSeatID = seatID
+
+echo fmt"Part 1: {largestSeatID}"
+
+var seats = newSeq[bool](largestSeatID + 1)
+for boardingPass in boardingPasses:
+  var
+    rowMin = 0
+    rowMax = 127
+    seatMin = 0
+    seatMax = 7
+  for chr in boardingPass[0 ..< ^3]:
+    (rowMin, rowMax) = binarySpacePartition(rowMin, rowMax, chr, rowChars)
+  let row = rowMin
+  for chr in boardingPass[^3 .. ^1]:
+    (seatMin, seatMax) = binarySpacePartition(seatMin, seatMax, chr, seatChars)
+  let
+    seat = seatMin
+    seatID = row * 8 + seat
+  seats[seatID] = true
+
+for idx, value in seats:
+  if not value and idx > 0 and idx < len(seats) and seats[idx-1] and seats[idx+1]:
+    echo fmt"Part 2: {idx}"
+    break
